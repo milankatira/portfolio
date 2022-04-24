4a8985666eddd1a86820f141116f4dc1311fa635
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-04-24 17:15:22 +0530

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
-
+import './App.scss';
 const App = () => {
   return (
-    <div>
-      App
+    <div className="app">
       <Navbar />
       <Header />
       <About />
