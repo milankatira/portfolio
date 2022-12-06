a36e7bfcc41b6ed7be5d35b5a6f476d5a46b5e1a
milankatira <milankatira26@gmail.com>
2022-12-06 08:57:00 +0530

    added chnages

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  darkMode: "class",
+  purge: {
+    enabled: true,
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./layouts/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
