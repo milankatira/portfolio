38b4bda5ecb4f81ed8738f76bf9db863164fa5ac
milankatira <milankatira26@gmail.com>
2022-12-07 22:36:17 +0530

    added contact us and skill

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import SectionTitle from './design/SectionTitle';
 
 const Skills = () => {
   return (
     <div>
+      <SectionTitle text="Skills" />
       <section className="relative pt-16 bg-blueGray-50">
         <div className="container mx-auto">
           <div className="flex flex-wrap items-center">
@@ -132,36 +134,6 @@ const Skills = () => {
             </div>
           </div>
         </div>
-        <footer className="relative bg-blueGray-50 pt-8 pb-6 mt-2">
-          <div className="container mx-auto px-4">
-            <div className="flex flex-wrap items-center md:justify-between justify-center">
-              <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-                <div className="text-sm text-blueGray-500 font-semibold py-1">
-                  Made with{" "}
-                  <a
-                    href="https://www.creative-tim.com/product/notus-js"
-                    className="text-blueGray-500 hover:text-gray-800"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Notus JS
-                  </a>{" "}
-                  by{" "}
-                  <a
-                    href="https://www.creative-tim.com"
-                    className="text-blueGray-500 hover:text-blueGray-800"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {" "}
-                    Creative Tim
-                  </a>
-                  .
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
       </section>
     </div>
   );
