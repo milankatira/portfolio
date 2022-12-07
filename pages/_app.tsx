64bdacd0fa1d4f63596aaccd76bd3308fecfe9b4
milankatira <milankatira26@gmail.com>
2022-12-07 23:15:13 +0530

    added project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import Navbar from "../layouts/Navbar";
-import Footer from "../layouts/Footer";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Navbar />
       <Component {...pageProps} />
-      <Footer/>
     </ThemeProvider>
   );
 }
