203fdb3918b58e2da1284c8dda69a62d4907b20f
milankatira <milankatira26@gmail.com>
2022-12-07 23:15:13 +0530

    added project

---
+++ components/Project.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type Props = {};
+
+export default function Project({}: Props) {
+  const projects = [1, 2, 3, 4];
+  return (
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Project
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          >
+            <img
+              src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+              className="w-48 h-48"
+            />
+
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">case study</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
+    </div>
+  );
+}
