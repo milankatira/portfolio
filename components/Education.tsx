143a08dba193cce36cccc0bb321391392c6cc4ae
milankatira <milankatira26@gmail.com>
2022-12-12 14:21:40 +0530

    added chnages

--- components/Education.tsx
+++ components/Education.tsx
@@ -33,8 +33,8 @@ const Education = () => {
             </div>
           </div>
           <div className="flex relative pb-20 sm:items-center md:w-2/3 mr-auto">
-            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-              <div className="h-4/5 w-1 bg-gray-200 pointer-events-none"></div>
+            <div className="h-4/5 w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
             </div>
             <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">
               2
