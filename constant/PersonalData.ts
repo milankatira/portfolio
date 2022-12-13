4478a112764434d3c588952fd54b9ca3334b240e
milankatira <milankatira26@gmail.com>
2022-12-13 22:01:02 +0530

    added resume and data clean up

--- constant/PersonalData.ts
+++ constant/PersonalData.ts
@@ -2,7 +2,7 @@ export const PersonalData = {
   socialMedia: [
     "https://medium.com/@milankatira26/",
     "https://www.linkedin.com/in/milan-katira/",
-    "https://github.com/cleverprogrammer/",
+    "https://github.com/milankatira/",
     "mailto:milankatira26@gmail.com",
   ],
 };
