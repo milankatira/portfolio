6537cbdf93b595e19c73137885fed4edb0d81d9b
milankatira <milankatira26@gmail.com>
2022-12-13 22:01:02 +0530

    added resume and data clean up

--- components/Abouts.tsx
+++ components/Abouts.tsx
@@ -84,7 +84,7 @@ function Abouts() {
                   }}
                   className="bg-blue-800 inline-flex items-center justify-center rounded-lg py-4 px-10 text-center text-base font-normal text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
                 >
-                  <a href="/img/css.png" download className="flex">
+                  <a href="/milan_katira.pdf" download className="flex">
                     Resume
                     <svg className="ml-4 h-6 w-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
                       <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
