b5aff56efab680e63733fa26807a6dc6b80b7963
milankatira <milankatira26@gmail.com>
2022-12-13 10:15:32 +0530

    added motion animation

---
+++ constant/Experiances.ts
@@ -0,0 +1,14 @@
+export const Experiances = [
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C510BAQFGhLqM90ohjQ/company-logo_200_200/0/1548588895541?e=2147483647&v=beta&t=n0dVEryhBW95RqALEoVo8bARfeA_58WoTv-XiM1p_Io",
+    position: "Junior Software Engineer",
+    skills: `<b>Skills:</b> MongoDB · Full-Stack Development · React.js · Node.js`,
+    duration: "Jan 2022 - Present",
+  },
+  {
+    img: "https://media-exp1.licdn.com/dms/image/C510BAQFGhLqM90ohjQ/company-logo_200_200/0/1548588895541?e=2147483647&v=beta&t=n0dVEryhBW95RqALEoVo8bARfeA_58WoTv-XiM1p_Io",
+    position: "Trainee Software Engineer",
+    skills: `<b>Skills:</b> MongoDB · Full-Stack Development · React.js · Node.js`,
+    duration: "Oce 2021 - Dec 2021",
+  },
+];
