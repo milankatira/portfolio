da2fc70a3d83f75fd9a4b8e01d13453ac852738f
milankatira <milankatira26@gmail.com>
2022-12-19 17:39:28 +0530

    added changes

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,15 +1,30 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { motion } from "framer-motion";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-hot-toast";
 import SectionTitle from "./design/SectionTitle";
-
 function ContactUs() {
 
-  const [name, setname] = useState("");
-  const [Email, setEmail] = useState("");
-  const [message, setmessage] = useState("");
-  const submitForm = () => {
+  const [loading, setloading] = useState(false);
 
-    window.open(`mailto:${Email}?&body=${encodeURIComponent(name)} : ${encodeURIComponent(message)}`);
+  const form = useRef();
+  const submitForm = (e: { preventDefault: () => void }) => {
+
+    e.preventDefault();
+    // @ts-ignore
+    emailjs.sendForm("service_a3j1ncp", "template_kyotqu5", form.current, "OELPiSYjiMixDJ_jb").then(
+      (result) => {
+
+        toast.success("thanks, email send successfully");
+
+      },
+      (error) => {
+
+        console.log(error.text);
+
+      },
+    );
 
   };
   return (
@@ -27,64 +42,81 @@ function ContactUs() {
               >
                 <div className="flex-auto p-5 lg:p-10">
                   <h4 className="text-2xl font-semibold">Want to work with us?</h4>
+                  {/* @ts-ignore */}
+                  <form ref={form} onSubmit={submitForm}>
+                    <div className="w-full mb-3 mt-8">
+                      <label
+                        className="block uppercase text-gray-600 dark:text-gray-300 text-xs font-bold mb-2"
+                        htmlFor="full-name"
+                      >
+                        Full Name
+                      </label>
+                      <input
+                        required
+                        type="text"
+                        name="name"
+                        className="border-0 px-3 py-3 placeholder-gray-300 dark:bg-gray-900 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Full Name"
+                      />
+                    </div>
 
-                  <div className="w-full mb-3 mt-8">
-                    <label
-                      className="block uppercase text-gray-600 dark:text-gray-300 text-xs font-bold mb-2"
-                      htmlFor="full-name"
-                    >
-                      Full Name
-                    </label>
-                    <input
-                      type="text"
-                      value={name}
-                      onChange={(e) => setname(e.target.value)}
-                      className="border-0 px-3 py-3 placeholder-gray-300 dark:bg-gray-900 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Full Name"
-                    />
-                  </div>
+                    <div className="w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-600 dark:text-gray-300 text-xs font-bold mb-2"
+                        htmlFor="email"
+                      >
+                        Email
+                      </label>
+                      <input
+                        required
+                        type="email"
+                        name="email"
+                        className="border-0 px-3 py-3 placeholder-gray-300 dark:bg-gray-900 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                        placeholder="Email"
+                      />
+                    </div>
 
-                  <div className="w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-600 dark:text-gray-300 text-xs font-bold mb-2"
-                      htmlFor="email"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      value={Email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="border-0 px-3 py-3 placeholder-gray-300 dark:bg-gray-900 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Email"
-                    />
-                  </div>
+                    <div className="w-full mb-3">
+                      <label
+                        className="block uppercase text-gray-600 dark:text-gray-300 text-xs font-bold mb-2"
+                        htmlFor="message"
+                      >
+                        Message
+                      </label>
+                      <textarea
+                        required
+                        rows={4}
+                        cols={80}
+                        name="message"
+                        className="border-0 px-3 py-3 placeholder-gray-300 dark:bg-gray-900 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                        placeholder="Type a message..."
+                      />
+                    </div>
+                    <div className="text-center mt-6">
+                      <button
+                        className="w-52 border-2 dark:border-white border-black hover:bg-black hover:text-white dark:hover:text-black dark:hover:bg-white duration-300 text-sm font-bold uppercase px-6 py-3 rounded  hover:shadow-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-300"
+                        type="submit"
+                      >
+                        {!loading
+                          ? <div className="mx-auto" aria-label="Loading..." role="status">
+                              <svg className="h-6 w-6 animate-spin" viewBox="3 3 18 18">
+                                <path
+                                  className="fill-gray-200"
+                                  d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+                                ></path>
+                                <path
+                                  className="fill-gray-800"
+                                  d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
+                                ></path>
+                              </svg>
+                            </div>:
+                          "Send Message"
+                        }
+                      </button>
 
-                  <div className="w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-600 dark:text-gray-300 text-xs font-bold mb-2"
-                      htmlFor="message"
-                    >
-                      Message
-                    </label>
-                    <textarea
-                      rows={4}
-                      cols={80}
-                      value={message}
-                      onChange={(e) => setmessage(e.target.value)}
-                      className="border-0 px-3 py-3 placeholder-gray-300 dark:bg-gray-900 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                      placeholder="Type a message..."
-                    />
-                  </div>
-                  <div className="text-center mt-6">
-                    <button
-                      onClick={submitForm}
-                      className="border-2 dark:border-white border-black hover:bg-black hover:text-white dark:hover:text-black dark:hover:bg-white duration-300 text-sm font-bold uppercase px-6 py-3 rounded  hover:shadow-2xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-300"
-                      type="button"
-                    >
-                      Send Message
-                    </button>
-                  </div>
+
+                    </div>
+                  </form>
                 </div>
               </motion.div>
             </div>
@@ -97,3 +129,10 @@ function ContactUs() {
 }
 
 export default ContactUs;
+function useStatw (second: any): [any, any]
+{
+
+  throw new Error("Function not implemented.");
+
+}
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "../styles/globals.css";
 import "../styles/work.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider attribute="class">
+      <Toaster/>
       <Component {...pageProps} />
     </ThemeProvider>
   );
