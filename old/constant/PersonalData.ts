99df496e35fd936b9e3b5c701971642cea547e62
milankatira <milankatira26@gmail.com>
2023-07-08 18:24:16 +0000

    reduce build size

--- old/constant/PersonalData.ts
+++
@@ -1,8 +0,0 @@
-export const PersonalData = {
-  socialMedia: [
-    "https://medium.com/@milankatira26/",
-    "https://www.linkedin.com/in/milan-katira/",
-    "https://github.com/milankatira/",
-    "mailto:milankatira26@gmail.com",
-  ],
-};
