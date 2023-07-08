c5f3dafde0fec1795ab6ec839677bd1b14eb9d89
milankatira <84834795+milankatira@users.noreply.github.com>
2023-07-08 18:24:16 +0000

    reduce build size

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,11 @@ import AboutWithSkills from "../components/About-with-skills";
 import Footer from "../components/Footer";
 import IntroTxt from "../components/Intro-txt";
 import Navbar from "../components/Navbar";
-import Numbers1 from "../components/Numbers";
 import PortfolioCustomColumn from "../components/Portfolio-custom-column";
 import Services4 from "../components/Services4";
 import Testimonials1 from "../components/Testimonials1";
-import BlogsThreeColum2 from "../components/Blogs-three-column2";
 import DarkTheme from "../layouts/Dark";
 import ContactSection from "../components/Contact-section";
-import ClientsBrands from "../components/Clients-brands";
 import CallToAction from "../components/Call-to-action";
 
 const Homepage2 = () => {
@@ -21,15 +18,11 @@ const Homepage2 = () => {
       <PortfolioCustomColumn column={2} filterPosition="left" />
       <Services4 halfBG />
       <AboutWithSkills />
-      {/* <Numbers1 /> */}
       <CallToAction />
       <Testimonials1 subBGLftstlParallaxie withBG overlay />
-      {/* <BlogsThreeColum2 /> */}
-      {/* <ClientsBrands subBG theme="dark" /> */}
       <div id="contactus">
         <ContactSection />
       </div>
-
       <Footer />
     </DarkTheme>
   );
