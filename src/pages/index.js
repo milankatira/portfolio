6d63d1aee621d986d49e2495a9f71ca779104236
milankatira <milankatira26@gmail.com>
2024-01-02 23:58:53 +0530

    fix navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,37 @@
 import React from "react";
 import AboutWithSkills from "../components/About-with-skills";
+import CallToAction from "../components/Call-to-action";
+import ContactSection from "../components/Contact-section";
 import Footer from "../components/Footer";
 import IntroTxt from "../components/Intro-txt";
 import Navbar from "../components/Navbar";
 import PortfolioCustomColumn from "../components/Portfolio-custom-column";
 import Services4 from "../components/Services4";
 import Testimonials1 from "../components/Testimonials1";
-import DarkTheme from "../layouts/Dark";
-import ContactSection from "../components/Contact-section";
-import CallToAction from "../components/Call-to-action";
-
+import LightTheme from "../layouts/Light";
+import DarkTheme from '../layouts/Dark';
 const Homepage2 = () => {
+  const navbarRef = React.useRef(null);
+  const logoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    var navbar = navbarRef.current;
+    if (window.pageYOffset > 300) {
+      navbar.classList.add("nav-scroll");
+    } else {
+      navbar.classList.remove("nav-scroll");
+    }
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
+  }, [navbarRef]);
   return (
     <DarkTheme>
-      <Navbar />
+      <Navbar nr={navbarRef} lr={logoRef} />
       <IntroTxt subBG />
       <PortfolioCustomColumn column={2} filterPosition="left" />
       <Services4 halfBG />
