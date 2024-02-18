eeb0a17fc68ec3196a280e95a7d4c390da04c88a
milankatira <milankatira26@gmail.com>
2024-02-18 12:49:46 +0530

    chnage base url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3002/api",
+  baseURL: "https://milankatira.vercel.app/api",
 });
 
 export default axiosInstance;
