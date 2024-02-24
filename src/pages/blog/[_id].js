e3d3d2a87a1a379be51c2250c6443fcb1a5168a7
milankatira <milankatira26@gmail.com>
2024-02-25 00:04:33 +0530

    try preview

--- src/pages/blog/[_id].js
+++ src/pages/blog/[_id].js
@@ -1,7 +1,7 @@
-import React from "react";
 import { NextSeo } from "next-seo";
+import Head from "next/head";
+import React from "react";
 import Markdown from "../../components/Markdown";
-import axiosInstance from "../../utils/axiosInstance";
 import { revertSlug } from "../../utils/slug";
 
 const BlogDetailsDark = ({ blogs }) => {
@@ -25,6 +25,17 @@ const BlogDetailsDark = ({ blogs }) => {
           type: "article",
         }}
       />
+      <Head>
+        <title>{blogs?.title}</title>
+        <meta name="description" content={blogs?.description} />
+        <meta property="og:title" content={blogs?.title} />
+        <meta property="og:description" content={blogs?.description} />
+        <meta property="og:image" content={blogs?.thumbnail} />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+        <meta property="og:url" content="https://milankatira.vercel.app" />
+        <meta property="og:type" content="article"></meta>
+      </Head>
       <section className="page-header blg">
         <div className="container">
           <div className="row justify-content-center">
