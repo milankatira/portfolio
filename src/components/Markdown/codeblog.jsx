1160158cd0e1e9baae91a68defd2ab2fff9201a5
milankatira26 <milan@instaservice.com>
2024-03-24 16:35:16 +0530

    added icons

--- src/components/Markdown/codeblog.jsx
+++ src/components/Markdown/codeblog.jsx
@@ -25,7 +25,7 @@ const Codeblog = ({Icon,node,children}) => {
     >
       <div className="card-body d-flex justify-content-between align-items-center px-4 py-2 border-bottom border-secondary">
         <div className="d-flex align-items-center gap-2">
-        {Icon}
+        <Icon/>
           <p className="text-muted small">
             {/* @ts-ignore  */}
             {node?.data?.meta}
